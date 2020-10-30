1d83f47387c2fe60ff458f20409a17e72f8b3ae5
Qiwen Gu <gqw0929@gmail.com>
2020-10-30 23:24:54 +0100

    Index improvment

--- index.php
+++ index.php
@@ -11,8 +11,10 @@
     <link rel="icon" href="logo.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="frontend.js"></script>
 </head>
 
 <body>
@@ -31,7 +33,7 @@
                 <h5>
                 This is a web-based tool aims at providing a guideline to software architects during their decision making about microservice migration and help them choose a suitable approach.
                 
-                    According to your specific requirement and the characteristics of your monolithic application, please select one or more choices for each section, and click "Search" for the result. You can also click "ShowAll" to see all avaliable documents.
+                    According to your specific requirement and the characteristics of your monolithic application, you can select radio buttons for each property, and decide whether to include this property or not. You can also exclude or leave it as neutral. "Include" button means you want to have it in the migration approach, and vice versa. Additionally, "Neutral" means that you do not care about this specific parameter and leave it free to the approach/ framework. You can move your cursor onto each section title and wait for a while, then an explain of the terminology will pop out to aid your selection. After selection, you can click "Search" button to perform searching in database and the tool will show you a list of matching contributions proposing suitable migration approaches or frameworks. Additionally, you are also allowed to select nothing and leave all parameters as neutral, then the web-based tool will provide you a whole list of contributions from database. Besides, the "Reset" button in the bottom right corner can clear all previous selections and reset the whole web page.
                 </h5>
             </div>
         </div>
@@ -55,7 +57,7 @@
                     <div class="row">
                         <div class="col-md-12" id="Process" name="Process" value="Process">
                             <h5 data-toggle="tooltip" data-placement="bottom" data-delay='{"show":"800"}' title="Strategy for implementing the migration, indicating the detailed approaches during migration">
-                                Process Strategy
+                                Process Strategy <i class="fa fa-question-circle"></i>
                             </h5>
                             <div class="row">
                                 <div class="col-md-6">
@@ -219,7 +221,7 @@
                     <div class="row">
                         <div class="col-md-12" id="Decomposition" name="Decomposition" value="Decomposition">
                             <h5 data-toggle="tooltip" data-placement="bottom" data-delay='{"show":"800"}' title="Strategy for decomposing the monolithic application, indicating the methodology for decomposotion">
-                                Decomposition Strategy
+                                Decomposition Strategy <i class="fa fa-question-circle"></i>
                             </h5>
                             <div class="row">
                                 <div class="col-md-6">
@@ -335,7 +337,7 @@
                     <div class="row">
                         <div class="col-md-12" id="Technique" name="Technique" value="Technique">
                             <h5 data-toggle="tooltip" data-placement="bottom" data-delay='{"show":"800"}' title="Detailed steps applied for migration, indicating the technical steps performed during decomposotion">
-                                Technique Type
+                                Technique Type <i class="fa fa-question-circle"></i>
                             </h5>
                             <div class="row">
                                 <div class="col-md-6">
@@ -471,7 +473,7 @@
                     <div class="row">
                         <div class="col-md-12" id="Applicability" name="Applicability" value="Applicability">
                             <h5 data-toggle="tooltip" data-placement="bottom" data-delay='{"show":"800"}' title="Applicable scenario and suitable system structure">
-                                Applicability
+                                Applicability <i class="fa fa-question-circle"></i>
                             </h5>
                             <div class="row">
                                 <div class="col-md-6">
@@ -534,7 +536,7 @@
                     <div class="row">
                         <div class="col-md-12" id="Input" name="Input" value="Input">
                             <h5 data-toggle="tooltip" data-placement="bottom" data-delay='{"show":"800"}' title="Avaliable input from the current application, indicating the necessary resources as pre-conditions">
-                                Input
+                                Avaliable Input <i class="fa fa-question-circle"></i>
                             </h5>
                             <div class="row">
                                 <div class="col-md-6">
@@ -669,7 +671,7 @@
                     <div class="row">
                         <div class="col-md-12" id="Output" name="Output" value="Output">
                             <h5 data-toggle="tooltip" data-placement="bottom" data-delay='{"show":"800"}' title="Resulting output after migration, indicating the final results after implementing this approach">
-                                Output
+                                Possible Output <i class="fa fa-question-circle"></i>
                             </h5>
                             <div class="row">
                                 <div class="col-md-6">
@@ -758,7 +760,7 @@
                     <div class="row">
                         <div class="col-md-12" id="Validation" name="Validation" value="Validation">
                             <h5 data-toggle="tooltip" data-placement="bottom" data-delay='{"show":"800"}' title="Validation type conducted in the contribution, indicating the reliability and correctness about this specific approach">
-                                Validation Type
+                                Validation Type <i class="fa fa-question-circle"></i>
                             </h5>
                             <div class="row">
                                 <div class="col-md-6">
@@ -868,7 +870,7 @@
                     <div class="row">
                         <div class="col-md-12" id="Quality" name="Quality" value="Quality">
                             <h5 data-toggle="tooltip" data-placement="bottom" data-delay='{"show":"800"}' title="Quality metrics or intension considered by the approach, act as characteristics which is desired by the user">
-                                Quality Metrics/Intention
+                                Quality Metrics/ Intention <i class="fa fa-question-circle"></i>
                             </h5>
                             <div class="row">
                                 <div class="col-md-6">
@@ -1031,10 +1033,9 @@
                     <input type="submit" formtarget="_blank" formaction="search.php" class="btn btn-outline-primary btn-block btn-lg" name="subject" id="Search" value="Search">
                 </div>
                 <div class="col-md-6">
-                    <button type="button" class="btn btn-outline-danger btn-block btn-lg" name="Reset" id="Reset" value="Reset" onClick="window.location.reload();">Reset</button>
+                    <button type="button" class="btn btn-outline-danger btn-block btn-lg" name="Reset" id="Reset" value="Reset" onClick="window.location.reload();" >Reset</button>
                 </div>
 
-               
             </div>
         </form>
     </div>
@@ -1043,6 +1044,9 @@
             $('[data-toggle="tooltip"]').tooltip();
         });
     </script>
+    
+
+   
 </body>
 
 </html>
